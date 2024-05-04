3d38aa5692ebff3c864b9bb8c2031dbc85025753
Clément <letouriste19@yahoo.fr>
2024-05-04 13:36:02 +0200

    push

--- led_test.vhd
+++ led_test.vhd
@@ -8,113 +8,150 @@ entity led_test is
     Port (
         CLK : in std_logic;  -- Signal d'horloge
 		  CLK1 : in std_logic;
-        LED_matrix : out std_logic_vector(4 downto 0) := "00000"; -- Matrice de sortie pour contrôler les LED
+        LED_matrix : buffer std_logic_vector(4 downto 0) := "00000"; -- Matrice de sortie pour contrôler les LED
         ground_matrix : buffer std_logic_vector(6 downto 0) := "1111111"; -- Matrice des grounds
 		BTN_up : in std_logic := '0'; -- Signal du bouton
 		BTN_down : in std_logic := '0';
 		vdd : out std_logic := '1';
 		segment_7 : buffer std_logic_vector(3 downto 0) := "0000";
-		vie : buffer std_logic_vector(2 downto 0) := "111"
+		life : buffer std_logic_vector(2 downto 0) := "111";
+		BTN_reset : in std_logic := '0'
     );
 end led_test;
 
 -- Architecture de l'entité
 architecture Behavioral of led_test is
-      signal ligne_counter : integer range 0 to 7 := 0;  -- Compteur de colonne pour le balayage
+      signal ligne_counter : integer range 0 to 2 := 0;  -- Compteur de colonne pour le balayage
 		signal prev_BTN_up_state : std_logic := '0';
 		signal prev_BTN_down_state : std_logic := '0';
+		signal prev_BTN_reset_state : std_logic := '0';
 		signal player : std_logic_vector(4 downto 0) := "00100";
 		signal player_pos : integer range 0 to 4 := 2;
 		signal ligne_counter_slow : integer range 0 to 7 :=0;
 		signal mur : std_logic_vector(4 downto 0) := "11011";
         signal ground_matrix_mur : std_logic_vector(6 downto 0);
 		  signal segment_7_count : natural range 0 to 9 := 0;
-		  signal game_over : boolean := false;
 		  
 		 signal pseudo_random : integer range 0 to 4;
-		 signal vie_count : integer range 0 to 3 := 3;
+		 signal life_count : integer range 0 to 3 := 3;
 		 signal speed : integer range 0 to 20 := 0;
+		 type states is (game_win, game_over, game_playing, game_reset);
+		 signal state : states := game_reset;
+		 signal state_signal : states := game_reset;
+		 signal game_reset_button : boolean := false;
 	 
 begin
 	-- Processus pour générer le nombre pseudo-aléatoire
 		 pseudo_random_generator : process(CLK)
 		 begin
 			  if rising_edge(CLK) then
-					if pseudo_random = 5 then
+					if pseudo_random + 1= 5 then
 						 pseudo_random <= 0;
 					else
 						 pseudo_random <= pseudo_random + 1;
 					end if;
 			  end if;
 		 end process pseudo_random_generator;
-	 
-    process(CLK)
-    begin
-        if rising_edge(CLK) then
-			if not game_over then
-            if BTN_up = '0' and prev_BTN_up_state = '1' then 
-                
-                player(player_pos) <= '0';
-                player((player_pos + 1)mod 5) <= '1';
-					 player_pos <= (player_pos + 1) mod 5;
-            end if;
-				
+		 
+		button : process(CLK)
+	begin
+		if rising_edge(CLK) then
+				if state = game_playing then 
+					game_reset_button <= false;
+				end if;
+			
+				if BTN_up = '0' and prev_BTN_up_state = '1' then 		 
+					if player_pos = 4 then
+						player(player_pos) <= '0';
+						player(0) <= '1';
+						player_pos <= 0;
+					else
+						player(player_pos) <= '0';
+						player(player_pos + 1) <= '1';
+						player_pos <= player_pos + 1;
+					end if;
+
+				end if;
+					
 				if BTN_down = '0' and prev_BTN_down_state = '1' then 
-                
-                player(player_pos) <= '0';
-                player((player_pos - 1) mod 5) <= '1';
-					 player_pos <= (player_pos - 1) mod 5;
+					if player_pos = 0 then
+						player(player_pos) <= '0';
+						player(4) <= '1';
+						player_pos <= 4;
+					else
+						player(player_pos) <= '0';
+						player((player_pos - 1)) <= '1';
+						player_pos <= (player_pos - 1);
+					end if;
+
 				end if;
 				
-				case vie_count is
+				if BTN_reset = '0' and prev_BTN_reset_state = '1' then
+					game_reset_button <= true;
+				end if;
+
+			prev_BTN_up_state <= BTN_up;
+			prev_BTN_down_state <= BTN_down;
+			prev_BTN_reset_state <= BTN_reset;
+		end if;
+	end process;
+	 
+	display : process(CLK)
+	begin 
+		if rising_edge(CLK) then
+			if state = game_playing then
+				case life_count is
 					when 3 =>
-						vie <= "111";
+						life <= "111";
                 when 2 =>
-                    vie <= "110";
+                    life <= "110";
                 when 1 =>
-                    vie <= "100";
+                    life <= "100";
                 when others =>
-                    vie <= "000";
+                    life <= "000";
             end case;
             
-            -- Afficher la colonne active
-            case ligne_counter is
-                when 0 => -- Première Ligne
-							
-							ground_matrix <= "1111110";
-							LED_matrix <= player;
-						 
-							
-					when 1 => -- Première Ligne
-
-						LED_matrix <= mur;
+				case ligne_counter is
+					when 0 =>
 						ground_matrix <= ground_matrix_mur;
-						                  
-						  
-                when others =>
-                    ligne_counter <= 0; -- Revenir à la première colonne si on dépasse la troisième colonne
-            end case;
-			else
+						LED_matrix <= mur;
+						
+					when 1 => 
+						ground_matrix <= "1111110";
+						LED_matrix <= player;
+					when others =>
+						ligne_counter <= 0; 
+				end case;
+				
+				ligne_counter <= ligne_counter + 1;
+			elsif state = game_over then
 				ground_matrix <= "0000000";
 				LED_matrix <= "11111";
-				vie <= "111";
+				life <= "100";
+			elsif state = game_win then
+				ground_matrix <= "0000000";
+				LED_matrix <= "11111";
+				life <= "010";
+				
 			end if;
-				
-				
-				segment_7 <= std_logic_vector(to_unsigned(segment_7_count, segment_7'length));
-				--vie <= std_logic_vector(to_unsigned(vie_count, vie'length));
-				ligne_counter <= ligne_counter + 1;
-				prev_BTN_up_state <= BTN_up;
-				prev_BTN_down_state <= BTN_down;
-        end if;
-    end process;
+		end if;
+	end process display;
 	 
 	 process(CLK1)
+	 variable mur_cst : std_logic_vector(4 downto 0) := "11111";
 	 begin
 		if rising_edge(CLK1) then
 		  if speed + segment_7_count*2 /= 30 then 
 				speed <= speed + 1;
 		  else
+		  
+		  if game_reset_button = true then 
+				ligne_counter_slow <= 0;
+				life_count <= 3;
+				segment_7_count <= 0;
+				state <= game_playing;
+			end if;
+				
 		
 			case ligne_counter_slow is
                 when 0 => -- Première Ligne
@@ -131,6 +168,7 @@ begin
 						  elsif pseudo_random = 4 then
 								mur <= "01111"; -- Trou à la cinquième position
 							end if;
+                     
 							
                 when 1 => -- Deuxième Ligne
                     ground_matrix_mur <= "1011111";
@@ -146,12 +184,18 @@ begin
                     ground_matrix_mur <= "1111110";
 						  if mur(player_pos) = '0' then
 								segment_7_count <= segment_7_count + 1;
+								segment_7 <= std_logic_vector(to_unsigned(segment_7_count, segment_7'length));
+								
+								if segment_7_count = 9 then
+									state <= game_win;
+								end if;
 							else
-								vie_count <= vie_count - 1;
-								if vie_count = 1 then
-									game_over <= true;
+								life_count <= life_count - 1;
+								if life_count = 1 then
+									state <= game_over;
 								end if;
 							end if;
+
 							
 							
 				  
